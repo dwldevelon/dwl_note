7419b19bd58b4ab061053540893d4dc1822571a1
wenlong.ding <wenlong.ding@zkjin.com>
2020-12-16 18:40:22 +0800

    default_commit

--- model/src/main/java/dwl/model/ModelApplication.java
+++ model/src/main/java/dwl/model/ModelApplication.java
@@ -1,13 +1,14 @@
 package dwl.model;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
 public class ModelApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(ModelApplication.class, args);
+//        SpringApplication.run(ModelApplication.class, args);
+        System.out.println("hello model");
     }
 
 }
