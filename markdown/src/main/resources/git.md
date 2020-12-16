4d69db5d8a3d66f3dfef6f0e99c9206161b874b1
wenlong.ding <wenlong.ding@zkjin.com>
2020-12-16 17:04:00 +0800

    second commit

---
+++ markdown/src/main/resources/git.md
@@ -0,0 +1,6 @@
+git 命令行记住密码  
+`git config --global credential.helper store`
+---
+git 设置代理   
+`git config http.proxy socks5://127.0.0.1:7000`
+---
---
+++ model/src/test/java/dwl/model/ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package dwl.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
